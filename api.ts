5222d5cb6766c88485d68dd8b9189b94da124e01
G00sebarry <krovorebyatkistok@gmail.com>
2026-01-21 16:57:41 +0300

    GOOGLE - фикс авториазции локалов

--- api.ts
+++ api.ts
@@ -248,7 +248,7 @@ export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: window.location.origin
+      redirectTo: 'https://plusyx.ru'  // 👈 Явно указываем продакшн URL
     }
   });
   
