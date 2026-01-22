a706981fd290a8dfcb59d6ad40d8f706494136b3
G00sebarry <krovorebyatkistok@gmail.com>
2026-01-23 00:55:57 +0300

    фикс 11 lucky

--- api/send-notification.js
+++ api/send-notification.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(200).json({ ok: true, message: 'Send notification endpoint' });
   }
@@ -28,5 +28,4 @@ module.exports = async (req, res) => {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
-};
-    
+}
